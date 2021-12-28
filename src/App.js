8aca0df823fe9d9f51463b3ad129f7f9a1795be0
martinazt <martinazera10@gmail.com>
2021-12-27 23:08:15 -0300

    Clase 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import MiModulo from './MiModulo'
+
 
 function App() {
+  // LO QUE VOY A VER EN PANTALLA
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Martina</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +22,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <MiModulo/>
     </div>
   );
 }
---
+++ src/MiModulo.js
@@ -0,0 +1,14 @@
+
+
+const MiModulo = () =>{
+    return (
+        <section>
+            <div>
+                <p>HOLA MUNDO</p>
+            </div>
+        </section>
+    )
+}
+
+
+export default MiModulo
